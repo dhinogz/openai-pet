41147e702b2621be67231bbb0cb824a6c6735401
dhinogz <davidhinojosagzz@gmail.com>
2023-03-20 01:49:37 -0600

    Davinci edit 001 feature

--- src/app/textdavinciedit001/textdavinciedit001.component.ts
+++ src/app/textdavinciedit001/textdavinciedit001.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Textdavinciedit001Service } from '../services/textdavinciedit001.service';
 
 @Component({
   selector: 'app-textdavinciedit001',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./textdavinciedit001.component.css']
 })
 export class Textdavinciedit001Component {
+  constructor(private textdavinciedit001: Textdavinciedit001Service) { }
 
+  ngOnInit(): void {
+  }
+
+  input : string = "";
+  instruction : string = "";
+  result : string = "";
+
+  postCompletition() {
+    let input = this.input
+    let instruction = this.instruction
+
+    var payload = {
+      model: "text-davinci-edit-001",
+      input: input,
+      instruction: instruction,
+      temperature: 0.6,
+    }
+
+    this.textdavinciedit001.postCompletion(payload)
+    .subscribe((data: any) => {
+      //alert(JSON.stringify(data));
+      console.log(data);
+      this.result = data.choices[0].text;
+    })
+
+  }
 }
