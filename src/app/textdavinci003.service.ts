264514df5df1d919e4a6b921c6b5b6104fd906ad
David Hinojosa <64158375+dhinogz@users.noreply.github.com>
2023-03-19 22:48:26 -0600

    Add environment variables functionality (#2)

--- src/app/textdavinci003.service.ts
+++ src/app/textdavinci003.service.ts
@@ -15,7 +15,7 @@ export class Textdavinci003Service {
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
-      'Authorization': "Bearer <api-key>"
+      'Authorization': "Bearer " + process.env['NG_APP_KEY'],
     })
   }
 
